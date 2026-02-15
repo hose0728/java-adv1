a4952b6d41159e07e6438d6d6281fe920a329d1f
hose Joo <hose0728@naver.com>
2026-02-15 17:22:32 +0900

    Add multiple examples demonstrating thread creation, usage, and logging utilities

---
+++ src/thread/start/test/StartTest4Main.java
@@ -0,0 +1,34 @@
+package thread.start.test;
+
+import static util.MyLogger.log;
+
+public class StartTest4Main {
+    public static void main(String[] args) {
+        Thread threadA = new Thread(() -> {
+            while (true) {
+                log("A");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        threadA.setName("Thread-A");
+        Thread threadB = new Thread(()->{
+            while (true) {
+                log("B");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        threadB.setName("Thread-B");
+        threadA.start();
+        threadB.start();
+    }
+
+
+}
